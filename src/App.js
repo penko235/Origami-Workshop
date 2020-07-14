091998a63b925fa5dffca5cd85d524215e85a81e
Penko Gadzhev <penko032@gmail.com>
2020-07-14 14:03:32 +0300

    feat: Implement origamis container

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from 'react'
 import Header from './components/header/index'
 import styles from './app.module.css'
+import Aside from './components/aside'
+import Origamis from './components/origamis'
 
 const App = () => {
   return (
     <div className={styles.app}>
       <Header />
+      <div className={styles.container}>
+        <Aside />
+        <Origamis />
+      </div>
     </div>
   )
 }
